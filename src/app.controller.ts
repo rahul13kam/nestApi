aca541b93e3127a17dd6478693fb7f135c5d678e
Rahul Kamble <rahulkamble@customfurnish.com>
2026-02-19 18:06:12 +0530

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('students')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  
+  getStudents(): any {
+    return this.appService.getStudents();
+  }
+}
---
+++ src/practice.js
@@ -0,0 +1,25 @@
+// function parent(){
+//     console.log("This is the first function");
+//    function child1(){
+//         console.log("This is the second function");
+//     }
+//     function child2(){
+//         console.log("This is the third function");
+//     }
+//     function child3(){
+//         console.log("This is the fourth function");
+//     }
+
+//     return{
+//        child2,
+//        child3
+//     }
+// }
+
+// console.log(parent());
+
+
+(function IIFI(){
+    var a  = 10;
+    console.log(a);
+})()
